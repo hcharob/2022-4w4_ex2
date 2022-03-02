13d17340981f9d8be433631dd168d87502957de5
Hervee Charbonneau <e1255799@cmaisonneuve.qc.ca>
2022-03-02 16:57:20 -0500

    Changements à la barre de recherche

--- searchform.php
+++ searchform.php
@@ -1,28 +1,11 @@
 <form class="recherche" method="get" action=" <?php echo get_home_url('/')?> ">
     <input type="text" class="recherche__input" name="s" value="<?php get_search_query(); ?>">
     <button class="recherche__bouton">
-        <svg width="32px" height="32px" viewBox="0 0 57 57" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-            color="#000000">
-            <g transform="translate(1 1)" fill-rule="evenodd">
-                <circle cx="5" cy="50" r="5">
-                    <animate attributeName="cy" begin="0s" dur="2.2s" values="50;5;50;50" calcMode="linear"
-                        repeatCount="indefinite"></animate>
-                    <animate attributeName="cx" begin="0s" dur="2.2s" values="5;27;49;5" calcMode="linear"
-                        repeatCount="indefinite"></animate>
-                </circle>
-                <circle cx="27" cy="5" r="5">
-                    <animate attributeName="cy" begin="0s" dur="2.2s" from="5" to="5" values="5;50;50;5"
-                        calcMode="linear" repeatCount="indefinite"></animate>
-                    <animate attributeName="cx" begin="0s" dur="2.2s" from="27" to="27" values="27;49;5;27"
-                        calcMode="linear" repeatCount="indefinite"></animate>
-                </circle>
-                <circle cx="49" cy="50" r="5">
-                    <animate attributeName="cy" begin="0s" dur="2.2s" values="50;50;5;50" calcMode="linear"
-                        repeatCount="indefinite"></animate>
-                    <animate attributeName="cx" from="49" to="49" begin="0s" dur="2.2s" values="49;5;27;49"
-                        calcMode="linear" repeatCount="indefinite"></animate>
-                </circle>
-            </g>
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+            color="#000">
+            <path fill-rule="evenodd"
+                d="M11.5 7a4.499 4.499 0 11-8.998 0A4.499 4.499 0 0111.5 7zm-.82 4.74a6 6 0 111.06-1.06l3.04 3.04a.75.75 0 11-1.06 1.06l-3.04-3.04z">
+            </path>
         </svg>
     </button>
 </form>
